c9269a0a4f57e54939293f4f1c56ccbd5c647df5
lvtyeflymoya <Zhanglele0526@126.com>
2026-04-13 10:23:50 +0800

    修改hero训练配置中的轮数以及模型保存配置，取消预训练权重加载

--- projects/HERO/configs_custom/hero_le90_r50_fpn_3x_dota.py
+++ projects/HERO/configs_custom/hero_le90_r50_fpn_3x_dota.py
@@ -27,7 +27,6 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     neck=dict(
         type='mmdet.FPN',
         in_channels=[256, 512, 1024, 2048],
